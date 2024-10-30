857fea4a11df44b69169582f1245abcea4c1392a
mirandalucas52 <mirandalucas52@gmail.com>
2024-10-30 14:50:28 +0100

    feat/add homepage

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useRef } from "react";
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+    const fileInputRef = useRef(null);
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            console.log("Fichier sélectionné:", file.name, file);
+            navigate("/recap");
+        }
+    };
+
+    return (
+        <div className="homepage">
+            <div className="wrap-content">
+                <img src="/logo.svg" alt="logo" />
+                <h1>Envoyez vos fichiers jusqu'à 2 Go</h1>
+                <h4>Dépose ton bazar, on s'occupe du reste !</h4>
+
+                <img
+                    className="dropfile-logo"
+                    src="/dropfile.jpg"
+                    alt="dropfile button"
+                    onClick={handleClick}
+                    style={{ cursor: "pointer" }}
+                />
+
+                <input
+                    type="file"
+                    ref={fileInputRef}
+                    onChange={handleFileChange}
+                    style={{ display: "none" }}
+                />
+
+                <p className="arrow-info-dropfile">
+                    <i class="fa-solid fa-arrow-up"></i>
+                </p>
+                <p className="info-dropfile">
+                    Cliquez ici pour déposer vos fichiers
+                </p>
+            </div>
+        </div>
+    );
 }
